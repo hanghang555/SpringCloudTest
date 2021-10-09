b5c00ed4c9df4d3bf452633fdddad28ee583b91a
hang1 <hang14756@163.com>
2021-10-09 16:41:31 +0800

    创建项目

---
+++ src/main/java/com/hang/springcloudlearn/SpringcloudlearnApplication.java
@@ -0,0 +1,13 @@
+package com.hang.springcloudlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudlearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudlearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hang/springcloudlearn/SpringcloudlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hang.springcloudlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudlearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
